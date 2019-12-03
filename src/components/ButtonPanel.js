2985bb3b11f8a9158c6f7f50cd32441e24c93ee1
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-12-03 14:08:46 -0700

    Updating props for buttonpanel

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -2,6 +2,7 @@ import React from "react";
 import Button from "./Button";
 export default class ButtonPanel extends React.Component {
   render() {
+    const light = "#b8bfc2";
     return (
       <div id="panel">
         <div class="group">
@@ -11,26 +12,26 @@ export default class ButtonPanel extends React.Component {
           <Button name="/" />
         </div>
         <div class="group">
-          <Button name="7" />
-          <Button name="8" />
-          <Button name="9" />
+          <Button name="7" color={light} />
+          <Button name="8" color={light} />
+          <Button name="9" color={light} />
           <Button name="X" />
         </div>
         <div class="group">
-          <Button name="4" />
-          <Button name="5" />
-          <Button name="6" />
+          <Button name="4" color={light} />
+          <Button name="5" color={light} />
+          <Button name="6" color={light} />
           <Button name="-" />
         </div>
         <div class="group">
-          <Button name="1" />
-          <Button name="2" />
-          <Button name="3" />
+          <Button name="1" color={light} />
+          <Button name="2" color={light} />
+          <Button name="3" color={light} />
           <Button name="+" />
         </div>
         <div class="group">
-          <Button name="0" />
-          <Button name="." />
+          <Button name="0" wide="true" color={light} />
+          <Button name="." color={light} />
           <Button name="=" />
         </div>
       </div>
