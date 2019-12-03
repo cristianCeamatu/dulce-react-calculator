b3b8008e488438b05582e974245af13f2ce0b1a9
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-12-03 14:08:19 -0700

    Adding more styling to button component: height, font-size, color

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,15 @@
 import React from "react";
 import PropTypes from "prop-types";
 export default function Button({ name, color, wide }) {
-  const styles = { backgroundColor: color, width: wide ? "50%" : "25%" };
+  const styles = {
+    backgroundColor: color,
+    width: wide ? "50%" : "25%",
+    height: 100,
+    fontSize: "2.5em"
+  };
   return <button style={styles}>{name}</button>;
 }
-Button.defaultProps = { color: "orange", wide: false };
+Button.defaultProps = { color: "#ffa420", wide: false };
 Button.propTypes = {
   result: PropTypes.string.isRequired,
   color: PropTypes.string.isRequired,
