83329e9b2b921fb6795174675ac494b472d9771c
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-12-03 13:34:06 -0700

    styling display component

--- src/components/Display.js
+++ src/components/Display.js
@@ -2,7 +2,7 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export default function Display({ result }) {
-  return <div>{result}</div>;
+  return <div id="display">{result}</div>;
 }
 
 Display.propTypes = { result: PropTypes.string };
