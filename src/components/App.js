d6662d3d03d50446b4762a57f94faff74befb622
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-12-05 20:29:36 -0700

    General functionality in App added

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,19 @@ import ButtonPannel from "./ButtonPanel";
 import calculate from "../logic/calculate";
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { total: null, next: null, operation: null };
+  }
+
+  handleClick(buttonName) {
+    this.setState(calculate(this.state, buttonName));
+  }
   render() {
     return (
       <div id="main">
-        <Display />
-        <ButtonPannel />
+        <Display result={this.state.total} />
+        <ButtonPannel clickHandler={this.handleClick} />
       </div>
     );
   }
