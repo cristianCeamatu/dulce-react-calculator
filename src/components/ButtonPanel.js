62ecce1e7e2e765b6ba23306188973e2dd4acda9
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-12-05 20:55:07 -0700

    handleClick implemented in each button

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,40 +1,44 @@
 import React from "react";
 import Button from "./Button";
-export default class ButtonPanel extends React.Component {
-  render() {
-    const light = "#b8bfc2";
-    return (
-      <div id="panel">
-        <div class="group">
-          <Button name="AC" />
-          <Button name="+/-" />
-          <Button name="%" />
-          <Button name="/" />
-        </div>
-        <div class="group">
-          <Button name="7" color={light} />
-          <Button name="8" color={light} />
-          <Button name="9" color={light} />
-          <Button name="X" />
-        </div>
-        <div class="group">
-          <Button name="4" color={light} />
-          <Button name="5" color={light} />
-          <Button name="6" color={light} />
-          <Button name="-" />
-        </div>
-        <div class="group">
-          <Button name="1" color={light} />
-          <Button name="2" color={light} />
-          <Button name="3" color={light} />
-          <Button name="+" />
-        </div>
-        <div class="group">
-          <Button name="0" wide="true" color={light} />
-          <Button name="." color={light} />
-          <Button name="=" />
-        </div>
+export default function ButtonPanel(props) {
+  const light = "#b8bfc2";
+  const { clickHandler } = props;
+  return (
+    <div id="panel">
+      <div class="group">
+        <Button name="AC" clickHandler={clickHandler} />
+        <Button name="+/-" clickHandler={clickHandler} />
+        <Button name="%" clickHandler={clickHandler} />
+        <Button name="/" clickHandler={clickHandler} />
       </div>
-    );
-  }
+      <div class="group">
+        <Button name="7" color={light} clickHandler={clickHandler} />
+        <Button name="8" color={light} clickHandler={clickHandler} />
+        <Button name="9" color={light} clickHandler={clickHandler} />
+        <Button name="X" clickHandler={clickHandler} />
+      </div>
+      <div class="group">
+        <Button name="4" color={light} clickHandler={clickHandler} />
+        <Button name="5" color={light} clickHandler={clickHandler} />
+        <Button name="6" color={light} clickHandler={clickHandler} />
+        <Button name="-" clickHandler={clickHandler} />
+      </div>
+      <div class="group">
+        <Button name="1" color={light} clickHandler={clickHandler} />
+        <Button name="2" color={light} clickHandler={clickHandler} />
+        <Button name="3" color={light} clickHandler={clickHandler} />
+        <Button name="+" clickHandler={clickHandler} />
+      </div>
+      <div class="group">
+        <Button
+          name="0"
+          wide="true"
+          color={light}
+          clickHandler={clickHandler}
+        />
+        <Button name="." color={light} clickHandler={clickHandler} />
+        <Button name="=" clickHandler={clickHandler} />
+      </div>
+    </div>
+  );
 }
