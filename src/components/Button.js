4259f8d434ed49b2ea62cdc5fc62212c48525124
Elizabeth Villalejos <nekobanpaia@gmail.com>
2019-12-05 20:50:03 -0700

    Clickhandler in button component added

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,15 +1,20 @@
 import React from "react";
 import PropTypes from "prop-types";
-export default function Button({ name, color, wide }) {
+export default function Button({ name, color, wide, clickHandler }) {
   const styles = {
     backgroundColor: color,
     width: wide ? "50%" : "25%",
     height: 100,
     fontSize: "2.5em"
   };
-  return <button style={styles}>{name}</button>;
+  return (
+    <button onClick={() => clickHandler({ name })} style={styles}>
+      {name}
+    </button>
+  );
 }
 Button.defaultProps = { color: "#ffa420", wide: false };
+
 Button.propTypes = {
   result: PropTypes.string.isRequired,
   color: PropTypes.string.isRequired,
